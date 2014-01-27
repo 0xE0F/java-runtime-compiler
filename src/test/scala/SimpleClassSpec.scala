d65d81a3abe92a0071063545fb3a050b8b6d161b
b.denis <b.denis@samsung.com>
2014-01-27 19:41:21 +0400

    A little improved tests

--- src/test/scala/SimpleClassSpec.scala
+++ src/test/scala/SimpleClassSpec.scala
@@ -5,29 +5,71 @@ import compiler._
 import org.scalatest.FlatSpec
 
 class SimpleClassSpec extends FlatSpec {
-   
-   def emitValidSource(className: String): String = {
-      val writer = new StringWriter
-      val output = new PrintWriter(writer)
-      
-      output.println("public class " + className + " {")
-      output.println("public void apply() { System.out.println(\"Called from: " + className + "\"); }" )
-      output.println("}")
+  var classCount = 0
+  def generateClassName = {
+    classCount = classCount + 1
+    "testClass_" + classCount
+  }
 
-      output.close
-      writer.toString
-   }
+  def emitValidSource(className: String): String = {
+    val builder = new StringBuilder
+    builder.append("public class " + className + " {")
+    builder.append("public void apply() { System.out.println(\"Called from: " + className + "\"); }")
+    builder.append("}")
+    builder.toString
+  }
 
-   "A Java Runtime Compiler class" should "compile simple class without error" in {
-         val compiler = new RuntimeJavaCompiler
-         val className = "SimpleClass"
-         val src = emitValidSource(className)
-         val obj = compiler.compile(className, src)
-         assert(obj != null)
+  def emitInvalidSource(className: String): String = {
+    val builder = new StringBuilder
+    builder.append("public clss " + className + " {")
+    builder.append("public void apply() { System.out.println(\"Called from: " + className + "\"); }")
+    builder.append("}")
+    builder.toString
+  }
+
+  def emitSourceWithoutDefaultCtor(className: String): String = {
+    val builder = new StringBuilder
+    builder.append("public class " + className + " {")
+    builder.append("private  " + className + "() { }")
+    builder.append("public void " + className +"(String name, Integer i) { System.out.println(\"Called from: \" + name); }")
+    builder.append("public void apply() { }")
+    builder.append("}")
+    builder.toString
+  }
+
+  def checkForThrowException[T<:Exception:Manifest](className: String, srcGen: String => String, ex: Class[T]) {
+      intercept[T] {
+      val compiler = new RuntimeJavaCompiler
+      val obj = compiler.compile(className, srcGen(className))
+    }
+  }
+
+  "A Java Runtime Compiler class" should "compile simple class without error" in {
+    val compiler = new RuntimeJavaCompiler
+    val className = generateClassName
+    val src = emitValidSource(className)
+    val obj = compiler.compile(className, src)
+    assert(obj != null)
+  }
+
+  it should "throw CompileException if an syntax error occur" in {
+      val className = generateClassName
+      checkForThrowException(className, emitInvalidSource, classOf[CompileException])
+  }
+
+  it should "throw IllegalAccessException if access rigth invalid"  in {
+    val className = generateClassName
+    checkForThrowException(className, emitSourceWithoutDefaultCtor, classOf[IllegalAccessException])
+  }
+
+  it should "throw InstantiationException if appropriate constructor not found" in {
+      intercept[InstantiationException] {
       }
+  }
 
-      it should "throw Exception if an error occur" in {
-         intercept[Exception] {
-         }
+  it should "throw ClassNotFoundException if class can't be loaded" in {
+      intercept[ClassNotFoundException] {
       }
+  }
+
 }
